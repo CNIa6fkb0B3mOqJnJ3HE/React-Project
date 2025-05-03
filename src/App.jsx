205abb3569a09d1840748feb22a9342855c0a417
vibodhalakshan <vibodhalakshan@gmail.com>
2025-05-03 14:37:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom"; // Corrected import for react-router-dom v6
 import { AuthProvider } from "./contexts/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
 
@@ -12,31 +12,31 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<AuthProvider>
-			<div className="overflow-hidden">
-				<Header />
-				<Routes>
-					{/* Public routes */}
-					<Route path="/" element={<Home />} />
-					<Route path="/signup" element={<Signup />} />
-					<Route path="/login" element={<Login />} />
+  return (
+    <AuthProvider>
+      <div className="overflow-hidden">
+        <Header />
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
 
-					{/* Protected routes */}
-					<Route
-						path="/profile"
-						element={
-							<PrivateRoute>
-								<Profile />
-							</PrivateRoute>
-						}
-					/>
-				</Routes>
-				<Sidebar />
-				<Footer />
-			</div>
-		</AuthProvider>
-	);
+          {/* Protected routes */}
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
